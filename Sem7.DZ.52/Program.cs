d20edf9cc48f4d93b5a5031d0e5b5d2c63ccc341
Балканов Богдан <89509494919@mail.ru>
2023-03-18 14:43:05 +0300

    18.03

--- Sem7.DZ.52/Program.cs
+++ Sem7.DZ.52/Program.cs
@@ -9,24 +9,24 @@ Console.Clear();
 Console.WriteLine("Введите число строк и столбцов");
 int m=int.Parse(Console.ReadLine());
 int n=int.Parse(Console.ReadLine());
-double [,] result =Masiv(m,n);
+int [,] result =Masiv(m,n);
 PrintArray(result);
-Arif(result);
+srArif(result);
 
 
-double[,]Masiv(int m,int n)
+int[,]Masiv(int m,int n)
 {
-    double[,] result = new double[m, n];
+    int [,] result = new int [m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(1, 9);
+            result [i, j] = new Random().Next(1, 9);
         }
     }
     return result;
 }
-void PrintArray(double[,] Array)
+void PrintArray(int[,] Array)
 {
     for (int i = 0; i < Array.GetLength(0); i++)
     {
@@ -37,17 +37,17 @@ void PrintArray(double[,] Array)
         Console.WriteLine();
     }
 }
-double[] Arif(double[,]array)
+void srArif(int[,]array)
 {
-    double [] res = new double [m];
+    int k = 0;
+    double sum = 0;
     for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
         {
-            for (int j = 0; j < array.GetLength(0); j++)
-            {
-                if()
-                {
-                    sum+=res;
-                }
-            }
+          sum+=array[j,i];
         }
+        Console.WriteLine($"{i+1} столбец ср.арифметеческое={sum/m:0.00}; ");
+        sum = 0;
+    }
 }
