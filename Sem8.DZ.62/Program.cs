1fc9a05aaad6d0a62fea540db1c993582c4ede1c
Балканов Богдан <89509494919@mail.ru>
2023-03-20 16:53:25 +0300

    20.03

---
+++ Sem8.DZ.62/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+/*  01 02 03 04
+    12 13 14 05
+    11 16 15 06
+    10 09 08 07 */
+Console.Clear();
+int [,] array = Masiv(4,4);
+PrintArray(array);
+
+
+int[,]Masiv(int m,int n)
+{
+    int count = 1;
+    int[,] result = new int[m, n];
+    int a = result.GetLength(0);
+    int b = result.GetLength(1);
+    for (int i = 0; i < a; i++)
+    {
+       
+        for (int j = 0; ;j++)
+        {
+            if(j < b)
+            {
+                result[i, j] = count++;
+
+            }
+            else
+            {
+                int k = result.GetLength(0);
+                a = result.GetLength(1);
+                b = k;
+            }
+        }
+    }
+    
+    return result;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
