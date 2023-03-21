ed9a54b50002da5ae91d086e4c57ba185fbb086d
Балканов Богдан <89509494919@mail.ru>
2023-03-21 21:04:42 +0300

    21.03

---
+++ Sem9.65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение M и N.
+// Напишите программу , которая выведет все натуральные числа в промежутке от M до N.
+Console.Clear();
+Console.WriteLine("Введите начальное значение :");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конечное значение :");
+int N = int.Parse(Console.ReadLine()!);
+Metod(M,N);
+/*for ( int i = M; i < N; i++)
+{
+    Console.Write($"{i}");
+}
+Console.WriteLine();
+Console.WriteLine(Metod(M,N));*/
+
+
+string Metod(int M, int N)
+{
+    if(N == M)
+    {
+        Console.WriteLine(N);
+        return M.ToString();
+    }
+    string s = Metod(M ,N - 1) + " " + N.ToString();
+    Console.WriteLine(s);
+    return s;
+}
