780e2c7a2fa6f0f543e6d02c1549ab963de44210
Балканов Богдан <89509494919@mail.ru>
2023-03-21 21:38:27 +0300

    21.03

---
+++ Sem9.69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа А и В,
+// и возврощает число А в целую степень В с помощью рекурсии.
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите число степени: ");
+int b = int.Parse(ReadLine()!);
+WriteLine($"{Recurs(a, b)}");
+
+int Recurs(int number, int stepen)
+{
+    if (stepen == 0)
+        return 1;
+    return ( number * Recurs(number, stepen - 1));
+}
