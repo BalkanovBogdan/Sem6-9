95fcf659bcab56d6878ba554b87eb62ee1017629
Балканов Богдан <89509494919@mail.ru>
2023-03-22 00:53:35 +0300

    21.03

---
+++ Sem9.DZ.64/Program.cs
@@ -0,0 +1,24 @@
+﻿/*  Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+using System;
+using static System.Console;
+Clear();
+WriteLine("Введите значение N: ");
+int n =int.Parse(ReadLine()!);
+MetodRecurs(n);
+
+string MetodRecurs(int number)
+{
+    int a = 1;
+    if(number == a)
+        return a.ToString();
+    string s =  number.ToString() + " " + MetodRecurs(number - 1);
+    WriteLine(s);
+    return s;
+}
---
+++ Sem9.DZ.66/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+using System;
+using static System.Console;
+Clear();
+Write("Введите начальное значение : ");
+int M = int.Parse(ReadLine()!);
+Write("Введите конечное значение :  ");
+int N = int.Parse(ReadLine()!);
+WriteLine(Metod( M, N ));
+
+int Metod(int M, int N)
+{
+    if(M == N + 1)
+        return 0;
+    return(M + Metod(M + 1, N));
+}
